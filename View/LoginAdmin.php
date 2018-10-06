8f4c9aa23aeeb52caf9b87eb9bff09998c6d4016
ngwdAlex <33274814+ngwdAlex@users.noreply.github.com>
2018-10-07 03:46:26 +0800

    login

--- View/LoginAdmin.php
+++ View/LoginAdmin.php
@@ -5,13 +5,33 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    
+
     <head>
         <meta charset="UTF-8">
         <title></title>
+        
     </head>
     <body>
+        <p>
+        <form method="post" action="LoginUser.php">
+            <p><label for="name">Username:</label>
+                <input type="text" name="name" id="name" size="15" /></p>
+            <p><label for="password">Password:</label>
+                <input type="password" name="password" id="password" size="15" /></p>
+            <button type="submit" name="login">Login</button>
+        </form>
         <?php
-        // put your code here
+        $username = trim($_POST['name']);
+        $pass = trim($_POST['password']);
+        
+        try{
+        
+        
+        }catch(Exception $ex){
+            echo "<p>Unauthorized User</p>";
+            echo '<a href="LoginUser.php">Back to previous page</a>';
+        }
         ?>
     </body>
 </html>
