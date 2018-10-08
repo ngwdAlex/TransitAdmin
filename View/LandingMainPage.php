fa054f37e548ccc270639ced8b7024df6b30fa7e
ngwdAlex <33274814+ngwdAlex@users.noreply.github.com>
2018-10-08 14:22:31 +0800

    added css but not implemented

--- View/LandingMainPage.php
+++ View/LandingMainPage.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="../CSS/css/sb-admin.css">
     </head>
-    <body style="align-content: center">
-        <h6>WELCOME</h6>
+    <body>
+        <h2 >WELCOME</h2>
         <a href="LogoutAdmin.php">LOGOUT</a>
     </body>
 </html>
--- View/LoginAdmin.php
+++ View/LoginAdmin.php
@@ -1,50 +1,56 @@
-<?php
-
-
-require __DIR__.'/vendor/autoload.php';
-
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\ServiceAccount;
-
-// This assumes that you have placed the Firebase credentials in the same directory
-// as this PHP file.
-$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/google-service-account.json');
-
-$firebase = (new Factory)
-    ->withServiceAccount($serviceAccount)
-    ->create();
-
-$auth = $firebase->getAuth();
-
-?>
 <html>
     
-
     <head>
+<!--        <script src="https://www.gstatic.com/firebasejs/5.5.3/firebase.js"></script>
+        <script>
+          // Initialize Firebase
+          var config = {
+            apiKey: "AIzaSyD4yBwQEfvjd-S9shlCvZap5E7SjQOQ1NE",
+            authDomain: "transitports-ee351.firebaseapp.com",
+            databaseURL: "https://transitports-ee351.firebaseio.com",
+            projectId: "transitports-ee351",
+            storageBucket: "transitports-ee351.appspot.com",
+            messagingSenderId: "1015664105253"
+          };
+          firebase.initializeApp(config);
+        </script>-->
+
         <meta charset="UTF-8">
         <title></title>
         
     </head>
     <body>
+        <?php
+            if ((!isset($_POST['name'])) || !isset($_POST['password'])) {
+        ?>
+            <h1>Log In</h1>
+            <p><h3>Please enter your email and password</h3></p>
         <p>
         <form method="post" action="LoginUser.php">
-            <p><label for="name">Username:</label>
-                <input type="text" name="name" id="name" size="15" /></p>
+            <p><label for="name">Email:</label>
+                <input type="text" name="email" id="email" size="15" /></p>
             <p><label for="password">Password:</label>
                 <input type="password" name="password" id="password" size="15" /></p>
             <button type="submit" name="login">Login</button>
         </form>
         <?php
-        $username = trim($_POST['name']);
-        $pass = trim($_POST['password']);
-        
-        try{
-        
-        
-        }catch(Exception $ex){
-            echo "<p>Unauthorized User</p>";
-            echo '<a href="LoginAdmin.php">Back</a>';
-        }
+            } else{
+                $email = trim($_POST['email']);
+                $pass = trim($_POST['password']);
+//                echo $email;
+//                echo $pass;
+//                firebase.auth().signInWithEmailAndPassword($email, $pass);
+                try{    
+                    echo "<p><h2>Welcome Admin!</h2></p>";
+                    echo '<p>';
+                    echo '<form action="LandingMainPage.php" method="post">';
+                    echo '<button type="submit" name="Confirm" value="Confirm">Confirm</button>';
+                    echo '</form></p>';
+                }catch(Exception $ex){
+                    echo "<p>Unauthorized User</p>";
+                    echo '<a href="LoginAdmin.php">Back</a>';
+                }
+            }
         ?>
     </body>
 </html>
